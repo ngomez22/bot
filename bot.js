7e848755b649b0c8847f8530a13ce83b0ec73063
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-01-18 22:01:06 -0500

    Load js dependencies

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
-var cmd = "C:/processing-3.2.3/processing-java.exe --sketch=C:/Users/Nicolás/Documents/bot/processing_sketch --run";
+var twit = require('twit');
+var snoocore = require('snoocore');
 var exec = require('child_process').exec;
+
+var cmd = "C:/processing-3.2.3/processing-java.exe --sketch=C:/Users/Nicolás/Documents/bot/processing_sketch --run";
 
 exec(cmd, done);
 
