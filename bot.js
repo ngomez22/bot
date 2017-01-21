a5613f97c269a6a0ad2b6553cd6474f552f57c6c
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-01-21 01:09:23 -0500

    Connection to reddit now succesful

--- bot.js
+++ bot.js
@@ -8,22 +8,7 @@ var exec = require('child_process').exec;
 //Setup
 var T = new Twit(twitterConfig);
 
-var reddit = new Snoocore({
-    // Unique string identifying the app
-    userAgent: 'node:bot:v1 (by /u/ngomez-bot)',
-    // It's possible to adjust throttle less than 1 request per second.
-    // Snoocore will honor rate limits if reached.
-    throttle: 300,
-    oauth: {
-        type: 'implicit',
-        key: '_sty7RuHMXLIfA',
-        redirectUri: 'http://localhost:3000',
-        // The OAuth scopes that we need to make the calls that we
-        // want. The reddit documentation will specify which scope
-        // is needed for evey call
-        scope: ['identity', 'read', 'vote']
-    }
-});
+var reddit = new Snoocore(redditConfig);
 
 
 var cmd = "C:/processing-3.2.3/processing-java.exe --sketch=C:/Users/Nicolás/Documents/bot/processing_sketch --run";
@@ -33,3 +18,7 @@ exec(cmd, done);
 function done() {
     console.log("Image generated.");
 }
+
+reddit('/api/v1/me').get().then(function(result) {
+  console.log(result); 
+});
