05725cad38bb1f5a06ffaa836846b27363b032d3
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-01-21 00:23:03 -0500

    Add config folder to store API keys

---
+++ config/twitter.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: '	hdkkvciYD5JMV7hvSs1ZGMr4s',
+    consumer_secret: '1pia0nNwRDVTu653TIp5bp4jkWU9NLqKWJbSiAbTXVyFuBOIiC',
+    access_token: '821913261322989569-pJeUtUtGWzMMdVdkiY4RXSaYvcBntSz',
+    access_token_secret: 'qZ6TX9Cdn7iY3momOFKNDZEUfivkIsTydfI9S9Mr4xySU',
+}
