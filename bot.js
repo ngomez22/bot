91616180e32132ae9cdc345a6259a7eb121c1a7a
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-02-07 19:02:02 -0500

    Fix image URL bug

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ var R = new Snoocore(redditConfig);
 function tweet() {
   //Find best /r/showerthoughts post
   R('/r/showerthoughts/top/?sort=top&t=day').listing().then(function(posts) {
-    console.log("thought: " + posts.children[0].data.title);
+    console.log("Thought: " + posts.children[0].data.title);
     return posts.children[0].data.title
   }).then(function(thought) {
     //Save the thought to the text file
@@ -28,16 +28,19 @@ function tweet() {
 
   //Find best /r/earthporn post
   R('/r/earthporn/top/?sort=top&t=day').listing().then(function(posts) {
-    console.log("pic: " + posts.children[0].data.url);
-    return posts.children[0].data.url
+    console.log("Pic: " + posts.children[0].data.url);
+    return posts.children[0].data.url.replace(/amp;/g, "");
   }).then(function(pic) {
     //Save the picture
     downloadPic(pic, "./processing_sketch/example-files/pic.jpg", function() {
       //Generate image to be posted
+      console.log("Succesfully saved image");
       const cmd = "C:/processing-3.2.3/processing-java.exe --sketch=C:/Users/Nicolás/Documents/bot/processing_sketch --run";
       exec(cmd, function(err, stdout, stderr) {
         //Handle any possible errors
-
+        if (err) {
+          console.log(err)
+        }
         //Post on twitter
         var b64pic = fs.readFileSync('./processing_sketch/output.png', {
           encoding: 'base64'
@@ -67,9 +70,6 @@ function tweet() {
 
 function downloadPic(uri, filename, callback) {
   request.head(uri, function(err, res, body) {
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
-
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
 };
